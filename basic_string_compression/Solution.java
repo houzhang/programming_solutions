5beef28465334eb41d9b2a8df33e96ce7c6e60f9
Houzhang Liang <houzhang@gmail.com>
2016-11-15 23:57:44 +0000

    added java solution for basic string compression

---
+++ basic_string_compression/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    
+    /**
+     * input:  "aaaabbbccdaa"
+     * output: "a3b2c1d0a1"
+     */
+    public static void main(String[] args) {
+        String output = "";
+        int counter = 0;
+        String s = "aaaabbbccdaa";
+        String[] sArray = s.split("");
+        for (int i=0; i<sArray.length; i++) {
+            if (i == 0) {
+                output += sArray[i];
+            } else {
+                if (sArray[i].equals(sArray[i-1])) {
+                    counter++;
+                } else {
+                    output += String.valueOf(counter);
+                    output += sArray[0];
+                    counter = 0;
+                }
+
+                if (i == sArray.length-1) {
+                    output += String.valueOf(counter);
+                }
+            }
+        }
+
+        System.out.println(output);
+    }
+
+}
